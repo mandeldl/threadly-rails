9915447cec03a53aa287a8b793376fe4f0165a7e
David Mandel <MandelDL@gmail.com>
2015-02-19 21:52:58 -0500

    on save redirect to index (which is the root path)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,4 +3,16 @@ class PostsController < ApplicationController
 		@new_post = Post.new
 		@all_posts = Post.order(created_at: :desc).all
 	end
+
+	def create
+		@post = Post.new(post_params)
+		if @post.save
+			redirect_to(:action=>'index')
+		end
+	end
+
+	private
+	def post_params
+		params.require(:post).permit(:comment)
+	end
 end
